070a569da796b9f824ff5becdf15328fbb4d6a90
Jagan.P <jagan.p@plantix.net>
2021-06-09 09:26:08 +0530

    New angular project without routing, app.module, angular.json, main.ts, explored app component, one-way (view to ts and ts to view) and two way data binding, theory of basic building block of angular, basic component styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-project';
+  second = 'second';
+  third = 'third';
+  isButtonDisabled = true;
+  paragraphClass = 'font';
+  inputValue='';
+  inputValueSecond = '';
+
+  inputValueKeydown(event) {
+    console.log(event.target.value);
+    this.inputValueSecond = event.target.value;
+  }
+
+  captureClick() {
+    console.log("click");
+    this.isButtonDisabled = !this.isButtonDisabled;
+    this.paragraphClass= this.paragraphClass+' background';
+    this.inputValue='captureClick';
+  }
 }
